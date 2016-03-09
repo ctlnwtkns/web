dbf7103020ebdcdc51ef906dae9c2b1c4685f78f
ctlnwtkns <ctlnwtkns@gmail.com>
2016-03-08 20:04:35 -0800

    execute renameTarot

--- renameTarot.py
+++ renameTarot.py
@@ -3,7 +3,7 @@
 
 import shutil, os, re
 
-os.chdir('/Users/caitlin/Documents/school/itc_110/abs_hw/ch9/waiteSmith')
+os.chdir('/Users/caitlin/Documents/school/itc_110/web/web/waiteSmith')
 
 #TODO: Loop over the files in the working directory.
 for folder, subFolder, fileName in os.walk('.'):
@@ -13,16 +13,18 @@ for folder, subFolder, fileName in os.walk('.'):
 		mo = nameRegex.search(files)
 		if mo != None:
 		#TODO: Get the different parts of the filename.	
-			int1 = mo.group(1) 
-			int2 = mo.group(2)
-			ext = mo.group(3)
-			if int2 != None:
-				newFile = nameRegex.sub(int1 + int2 + ext, files)
+			no1 = mo.group(1)
+			no2 = mo.group(2)
+			name = mo.group(9)
+			if no2 != None:
+				newFile = nameRegex.sub(no1 + no2 + name, files)
 			else:
-				newFile = nameRegex.sub(int1 + ext, files)
+				newFile = nameRegex.sub(no1 + name, files)
+						
 			#TODO: Get the full, absolute file paths.
 			oldPath = os.path.join(os.path.abspath(folder), files)
 			newPath = os.path.join(os.path.abspath(folder), newFile)
 			#TODO: Rename the files.
 			print('Renaming "%s" to "%s"...' % (files, newFile))
+			#print('Renaming "%s" to "%s"...' % (oldPath, newPath))
 			shutil.move(oldPath, newPath) #uncomment after testing
